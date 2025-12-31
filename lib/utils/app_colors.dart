8e3cd0febafa0e8c88fe76acbdb795e66b5e1b73
OtakaEmara <osama7emara@gmail.cm>
2025-12-31 19:07:23 +0200

    + added splash screen

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color black = Color(0xFF202020);
+  static const Color gold = Color(0xFFE2BE7F);
+  static const Color grey = Color(0xFF707070);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkWhite = Color(0xFFFFF5E3);
+  static const Color brown = Color(0xFF946107);
+}
