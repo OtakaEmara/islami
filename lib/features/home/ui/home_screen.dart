3c7efb76b4eea9f7d94f07191906ea31d1ead6db
OtakaEmara <osama7emara@gmail.cm>
2026-01-07 14:30:55 +0200

    + added home navigation bat to the screen

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,18 +1,79 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../../utils/app_colors.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    BottomNavigationBarItem bottomNavDesign({required String imageName, required int index}){
+      return BottomNavigationBarItem(
+          icon: currentIndex == index
+              ? Container(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.04,vertical: height * 0.006),
+              decoration: BoxDecoration(
+                color: AppColors.black.withOpacity(0.6),
+                borderRadius: BorderRadius.circular(height * 0.07),
+              ),
+              child: ImageIcon(AssetImage('assets/images/$imageName.png'))
+          )
+              : ImageIcon(AssetImage('assets/images/$imageName.png')),
+          label: ''
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("Home"),
-      ),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: AppColors.gold,
+          ),
+          child: BottomNavigationBar(
+            selectedItemColor: AppColors.white,
+            unselectedItemColor: AppColors.black,
+            currentIndex: currentIndex,
+            onTap: (index){
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            items: [
+              bottomNavDesign(
+                  imageName: 'quran_tab',
+                  index: 0
+              ),
+              bottomNavDesign(
+                  imageName: 'hadeth_tab',
+                  index: 1
+              ),
+              bottomNavDesign(
+                  imageName: 'sebha_tab',
+                  index: 2
+              ),
+              bottomNavDesign(
+                  imageName: 'radio_tab',
+                  index: 3
+              ),
+              bottomNavDesign(
+                  imageName: 'time_tab',
+                  index: 4
+              ),
+            ],
+          ),
+        )
     );
   }
 }
